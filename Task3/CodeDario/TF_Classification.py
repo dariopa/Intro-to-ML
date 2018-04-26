d5dc2f71ae10f8f58c382e3b31e0bbaf6bf95a37
dariopa <dariopa@ethz.ch>
2018-04-26 13:24:42 +0200

    Darios first code for task 3

---
+++ Task3/CodeDario/TF_Classification.py
@@ -0,0 +1,154 @@
+import os
+import numpy as np
+import tensorflow as tf
+import time
+import pandas as pd
+from matplotlib.pyplot import imshow
+import matplotlib.pyplot as plt
+from sklearn.utils import shuffle
+from utils_output import PrintOutput
+from utils_NN import NeuralNetworks as NN
+from utils_training import train, predict
+
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True #Do not assign whole gpu memory, just use it on the go
+config.allow_soft_placement = True #If an operation is not defined in the default device, let it execute in another.
+
+timestr = time.strftime("%Y%m%d-%H%M%S")
+
+random_seed = 123
+np.random.seed(random_seed)
+tf.set_random_seed(random_seed)
+
+# Data Path
+CallFolder = '../Raw_Data/'
+StoreFolder ='Final_Results/'
+if not os.path.isdir(StoreFolder):
+    os.makedirs(StoreFolder)
+
+StoreFolder_selfeval ='Selfeval_Results/'
+if not os.path.isdir(StoreFolder_selfeval):
+    os.makedirs(StoreFolder_selfeval)
+
+#########################################################
+# Decide whether self-evaluation or final submission
+final_submission = True
+Train_split = 8./10
+
+# Hyperparameters
+epochs = 100
+batch_size = 64
+learning_rate = 0.001
+
+#########################################################
+# LOAD AND SHUFFLE DATA!
+DataTrain = np.array(pd.read_hdf(CallFolder + "train.h5", "train"))
+X_train = DataTrain[:, 1:]
+y_train = DataTrain[:, 0]
+classes = np.max(y_train)+1
+
+X_test = np.array(pd.read_hdf(CallFolder + "test.h5", "test"))
+print('X_train:   ', X_train.shape, end=' ||  ')
+print('y_train:   ', y_train.shape)
+print('X_test:    ', X_test.shape)
+
+(X_train, y_train) = shuffle(X_train, y_train)
+
+#########################################################
+# TRAIN DATA
+if final_submission == True:
+
+    print('Shape of X_train:', X_train.shape)
+    print('Shape of y_train:', y_train.shape)
+    print('Shape of X_test:', X_test.shape, '\n')
+
+    ##################
+    # CREATE GRAPH
+    ## create a graph
+    g = tf.Graph()
+    with g.as_default():
+        tf.set_random_seed(random_seed)
+        ## build the graph
+        NN.build_NN(classes, learning_rate)
+
+    ##################
+    # TRAINING & PREDICTION
+    print()
+    print('Training... ')
+    with tf.Session(graph=g, config=config) as sess:
+        [avg_loss_plot, test_accuracy_plot] = train(sess=sess, epochs=epochs,
+                                                    random_seed=random_seed,
+                                                    batch_size=batch_size,                                                                 
+                                                    training_set=(X_train, y_train),
+                                                    test_set=None)
+
+        np.save(os.path.join(StoreFolder, timestr + '_avg_loss_plot.npy'), avg_loss_plot)
+
+        y_test_pred = predict(sess, X_test)
+    
+    
+    PrintOutput(y_test_pred, os.path.join(StoreFolder, timestr + '_' + str(epochs) + '_' + str(epochs) + '_y_test.csv'))
+
+else:
+    samples = len(X_train)
+    X_train_selfeval = X_train[0:int(Train_split * samples), :]
+    y_train_selfeval = y_train[0:int(Train_split * samples)]
+    print('Shape of X_train:', X_train_selfeval.shape)
+    print('Shape of y_train:', y_train_selfeval.shape)
+
+    X_test_selfeval = X_train[int(Train_split * samples):samples, :]
+    y_test_selfeval = y_train[int(Train_split * samples):samples]
+    print('Shape of X_test:', X_test_selfeval.shape)
+    print('Shape of y_test:', y_test_selfeval.shape)
+
+    ##################
+    # CREATE GRAPH
+    ## create a graph
+    g = tf.Graph()
+    with g.as_default():
+        tf.set_random_seed(random_seed)
+        ## build the graph
+        NN.build_NN(classes, learning_rate)
+
+    ##################
+    # TRAINING & PREDICTION
+    print()
+    print('Training... ')
+    with tf.Session(graph=g, config=config) as sess:
+        [avg_loss_plot, test_accuracy_plot] = train(sess=sess, epochs=epochs,
+                                                    random_seed=random_seed,
+                                                    batch_size=batch_size,                                                                 
+                                                    training_set=(X_train_selfeval, y_train_selfeval),
+                                                    test_set=(X_test_selfeval, y_test_selfeval))
+
+        np.save(os.path.join(StoreFolder_selfeval, timestr + '_avg_loss_plot.npy'), avg_loss_plot)
+        np.save(os.path.join(StoreFolder_selfeval, timestr + '_test_accuracy_plot.npy'), test_accuracy_plot)
+
+        y_test_pred = predict(sess, X_test)
+    
+    
+    PrintOutput(y_test_pred, os.path.join(StoreFolder_selfeval, timestr + '_' + str(epochs) + '_' + str(epochs) + '_y_test.csv'))
+
+##################
+# POSTPROCESS
+
+plt.figure(1)
+plt.plot(range(1, len(avg_loss_plot) + 1), avg_loss_plot)
+plt.title('Training loss')
+plt.xlabel('Epoch')
+plt.ylabel('Average Training Loss')
+if final_submission == True:
+    plt.savefig(os.path.join(StoreFolder,timestr + '_' + str(epochs) + '_' + str(batch_size) + '_TrainLoss.jpg'))
+else:
+    plt.savefig(os.path.join(StoreFolder_selfeval,timestr + '_' + str(epochs) + '_' + str(batch_size) + '_TrainLoss.jpg'))
+
+if final_submission == False:
+    plt.figure(2)
+    plt.plot(range(1, len(test_accuracy_plot) + 1), test_accuracy_plot, label='Test Accuracy')
+    plt.title('Test Accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    plt.savefig(os.path.join(StoreFolder_selfeval,timestr + '_' + str(epochs) + '_' + str(batch_size) + '_TestAccuracy.jpg'))
+
+print('\nJob Done!')
---
+++ Task3/CodeDario/utils_training.py
@@ -0,0 +1,72 @@
+import os
+import numpy as np
+import tensorflow as tf
+from sklearn.utils import shuffle
+from PIL import Image
+import time
+
+
+def batch_generator(X, y, batch_size, random_seed, shuffle=False):
+    idx = np.arange(y.shape[0])
+    
+    if shuffle:
+        rng = np.random.RandomState(random_seed)
+        rng.shuffle(idx)
+        X = X[idx]
+        y = y[idx]
+    
+    for i in range(0, X.shape[0], batch_size):
+        yield (X[i:i+batch_size, :], y[i:i+batch_size])
+
+def train(sess, epochs, random_seed, batch_size, training_set,
+          test_set=None, initialize=True, shuffle=True,
+          dropout=0.5):
+
+    X_data_train = np.array(training_set[0])
+    y_data_train = np.array(training_set[1])
+    avg_loss_plot = []
+    test_accuracy_plot = []
+
+    saver = tf.train.Saver()
+
+    # initialize variables
+    if initialize:
+        sess.run(tf.global_variables_initializer())
+
+    for epoch in range(1, epochs+1):
+        avg_loss = 0.0   
+        start_time = time.time()
+        batch_gen = batch_generator(X_data_train, y_data_train, batch_size, random_seed, shuffle=shuffle)
+        for i,(batch_x, batch_y) in enumerate(batch_gen):
+            feed = {'tf_x:0': batch_x, 
+                    'tf_y:0': batch_y, 
+                    'fc_keep_prob:0': dropout}
+            loss, _ = sess.run(['cross_entropy_loss:0', 'train_op'], feed_dict=feed)
+            avg_loss += loss
+
+        avg_loss_plot.append(np.mean(avg_loss))
+        print('\nEpoch %02d Training Avg. Loss: %7.3f' % (epoch, avg_loss), end=' ')
+
+        if test_set is not None:
+            feed = {'tf_x:0': test_set[0],
+                    'tf_y:0': test_set[1],
+                    'fc_keep_prob:0':1.0}
+            test_acc = 100*sess.run('accuracy:0', feed_dict=feed)
+            test_accuracy_plot.append(test_acc)
+            print(' Test Acc: %7.3f%%' % test_acc)
+        else:
+            print()
+
+        end_time = time.time()
+        print("Total time taken this loop [s]: ", end_time - start_time)
+        if epoch == 1:
+            print('Termination time will be:  ', time.ctime(start_time + (end_time - start_time)*epochs))
+
+    return avg_loss_plot, test_accuracy_plot
+
+def predict(sess, X, return_proba=False):
+    feed = {'tf_x:0': X, 'fc_keep_prob:0': 1.0}
+    if return_proba:
+        return sess.run('probabilities:0', feed_dict=feed)
+    else:
+        return sess.run('labels:0', feed_dict=feed)
