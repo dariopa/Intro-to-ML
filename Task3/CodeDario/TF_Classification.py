7f64b6212dcd55e6a51067e7f9da5b0dbae159e1
dariopa <dariopa@ethz.ch>
2018-04-30 19:17:55 +0200

    store and load best model, plus random sample selector

--- Task3/CodeDario/TF_Classification.py
+++ Task3/CodeDario/TF_Classification.py
@@ -1,14 +1,17 @@
 import os
+import shutil
 import numpy as np
 import tensorflow as tf
 import time
 import pandas as pd
+import random
 from matplotlib.pyplot import imshow
 import matplotlib.pyplot as plt
 from sklearn.utils import shuffle
+from sklearn.preprocessing import normalize
 from utils_output import PrintOutput
 from utils_NN import NeuralNetworks as NN
-from utils_training import train, predict
+from utils_training import train, predict, load, save
 from utils_preprocessing import centering
 
 config = tf.ConfigProto()
@@ -31,16 +34,27 @@ StoreFolder_selfeval ='Selfeval_Results/'
 if not os.path.isdir(StoreFolder_selfeval):
     os.makedirs(StoreFolder_selfeval)
 
+StoreFolder_Model ='Models/'
+if os.path.exists(StoreFolder_Model) and os.path.isdir(StoreFolder_Model):
+    shutil.rmtree(StoreFolder_Model)
+if not os.path.isdir(StoreFolder_selfeval):
+    os.makedirs(StoreFolder_selfeval)
+
 #########################################################
 # Decide whether self-evaluation or final submission
-final_submission = True
-Train_split = 9./10
+final_submission = False
+Test_split = 9./10
+Val_split = 9./10
 
 # You want to preprocess the data?
 preprocessing = True
 
+# how many random samples?
+random_selection = True
+random_samples = 30500
+
 # Hyperparameters
-epochs = 81
+epochs = 10
 batch_size = 32
 learning_rate = 0.001
 params = 88
@@ -50,94 +64,132 @@ params = 88
 DataTrain = np.array(pd.read_hdf(CallFolder + "train.h5", "train"))
 X_train = DataTrain[:, 1:]
 y_train = DataTrain[:, 0]
-classes = np.max(y_train)+1
+classes = np.max(y_train) + 1
 
 X_test = np.array(pd.read_hdf(CallFolder + "test.h5", "test"))
+print('Unpreprocessed Data')
 print('X_train:   ', X_train.shape, end=' ||  ')
 print('y_train:   ', y_train.shape)
-print('X_test:    ', X_test.shape)
+print('X_test:    ', X_test.shape, '\n')
 
 (X_train, y_train) = shuffle(X_train, y_train)
 
 #########################################################
-# TRAIN DATA
+# RANDOMSELECTOR
+if random_selection == True:
+    samples = len(X_train)
+    random_list = []
+    for i in range(random_samples):
+        random_list.append(random.randint(0, samples-1))
+
+    X_train = X_train[random_list, :]
+    y_train = y_train[random_list]
+    print('Randomly selected data')
+    print('X_train:   ', X_train.shape, end=' ||  ')
+    print('y_train:   ', y_train.shape)
+    print('X_test:    ', X_test.shape, '\n')
+
+#########################################################
+# FINAL DATA
 if final_submission == True:
     if preprocessing == True:
         X_train, X_test = centering(X_train, X_test)
+        X_train = normalize(X_train)
+        X_test = normalize(X_test)
 
+    samples = len(X_train)
+    X_valid = X_train[int(Val_split * samples):samples, :]
+    y_valid = y_train[int(Val_split * samples):samples]
+    X_train = X_train[0:int(Val_split * samples), :]
+    y_train = y_train[0:int(Val_split * samples)]
+    print('Final Data')
     print('Shape of X_train:', X_train.shape)
     print('Shape of y_train:', y_train.shape)
-    print('Shape of X_test:', X_test.shape, '\n')
+    print('Shape of X_valid:', X_valid.shape)
+    print('Shape of y_valid:', y_valid.shape, '\n')
 
     ##################
     # CREATE GRAPH
-    ## create a graph
     g = tf.Graph()
     with g.as_default():
-        tf.set_random_seed(random_seed)
-        ## build the graph
+        # build the graph
         NN.build_NN(classes, learning_rate, params)
 
     ##################
-    # TRAINING & PREDICTION
+    # TRAINING
     print()
     print('Training... ')
     with tf.Session(graph=g, config=config) as sess:
-        [avg_loss_plot, test_accuracy_plot] = train(sess=sess, epochs=epochs,
-                                                    random_seed=random_seed,
-                                                    batch_size=batch_size,                                                                 
-                                                    training_set=(X_train, y_train),
-                                                    test_set=None)
+        [avg_loss_plot, valid_accuracy_plot, test_accuracy_plot] = train(path=StoreFolder_Model, sess=sess, epochs=epochs,
+                                                                         random_seed=random_seed,
+                                                                         batch_size=batch_size,                                                                 
+                                                                         training_set=(X_train, y_train),
+                                                                         validation_set=(X_valid, y_valid),
+                                                                         test_set=None)
 
         np.save(os.path.join(StoreFolder, timestr + '_avg_loss_plot.npy'), avg_loss_plot)
+    del g
 
+    ##################
+    # CREATE GRAPH
+    g2 = tf.Graph()
+    with g2.as_default():
+        # build the graph
+        NN.build_NN(classes, learning_rate, params)
+
+        # Saver
+        saver = tf.train.Saver()
+
+    ##################
+    # PREDICTION
+    with tf.Session(graph=g2, config=config) as sess:
+        epoch = np.argmax(valid_accuracy_plot) + 1
+        load(saver=saver, sess=sess, epoch=epoch, path=StoreFolder_Model)
         y_test_pred = predict(sess, X_test)
-    
-    
+
     PrintOutput(y_test_pred, os.path.join(StoreFolder, timestr + '_' + str(epochs) + '_' + str(batch_size) + '_' + str(params) + '_y_test.csv'))
 
+#################################################################################################
+#################################################################################################
+# SELFEVALUATION
 else:
     samples = len(X_train)
-    X_train_selfeval = X_train[0:int(Train_split * samples), :]
-    y_train_selfeval = y_train[0:int(Train_split * samples)]
+    X_train_selfeval = X_train[0:int(Test_split * samples), :]
+    y_train_selfeval = y_train[0:int(Test_split * samples)]
+    X_test_selfeval = X_train[int(Test_split * samples):samples, :]
+    y_test_selfeval = y_train[int(Test_split * samples):samples]
+    print('Self-evaluation data')
     print('Shape of X_train:', X_train_selfeval.shape)
     print('Shape of y_train:', y_train_selfeval.shape)
-
-    X_test_selfeval = X_train[int(Train_split * samples):samples, :]
-    y_test_selfeval = y_train[int(Train_split * samples):samples]
     print('Shape of X_test:', X_test_selfeval.shape)
     print('Shape of y_test:', y_test_selfeval.shape)
 
     if preprocessing == True:
         X_train_selfeval, X_test_selfeval = centering(X_train_selfeval, X_test_selfeval)
+        X_train_selfeval = normalize(X_train_selfeval)
+        X_test_selfeval = normalize(X_test_selfeval)
 
     ##################
-    # CREATE GRAPH
-    ## create a graph
+    # CREATE GRAPH TRAINING
     g = tf.Graph()
     with g.as_default():
-        tf.set_random_seed(random_seed)
-        ## build the graph
+        # build the graph
         NN.build_NN(classes, learning_rate, params)
 
     ##################
-    # TRAINING & PREDICTION
+    # TRAINING
     print()
     print('Training... ')
     with tf.Session(graph=g, config=config) as sess:
-        [avg_loss_plot, test_accuracy_plot] = train(sess=sess, epochs=epochs,
-                                                    random_seed=random_seed,
-                                                    batch_size=batch_size,                                                                 
-                                                    training_set=(X_train_selfeval, y_train_selfeval),
-                                                    test_set=(X_test_selfeval, y_test_selfeval))
+        [avg_loss_plot, valid_accuracy_plot, test_accuracy_plot] = train(path=StoreFolder_Model, sess=sess, epochs=epochs,
+                                                                         random_seed=random_seed,
+                                                                         batch_size=batch_size,                                                                 
+                                                                         training_set=(X_train_selfeval, y_train_selfeval),
+                                                                         validation_set=None,
+                                                                         test_set=(X_test_selfeval, y_test_selfeval))
 
         np.save(os.path.join(StoreFolder_selfeval, timestr + '_avg_loss_plot.npy'), avg_loss_plot)
         np.save(os.path.join(StoreFolder_selfeval, timestr + '_test_accuracy_plot.npy'), test_accuracy_plot)
-
-        y_test_pred = predict(sess, X_test)
-    
-    
-    PrintOutput(y_test_pred, os.path.join(StoreFolder_selfeval, timestr + '_' + str(epochs) + '_' + str(batch_size) + '_' + str(params) + '_y_test.csv'))
 
 ##################
 # POSTPROCESS
