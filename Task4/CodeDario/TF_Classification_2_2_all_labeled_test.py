0ba4c97de1e0ff8d0aff8315960a4bfdeef43f2b
dariopa <dariopa@ethz.ch>
2018-05-28 23:44:44 +0200

    test

--- Task4/CodeDario/TF_Classification_2_2_all_labeled_test.py
+++ Task4/CodeDario/TF_Classification_2_2_all_labeled_test.py
@@ -54,10 +54,10 @@ Val_split = 9.5/10
 preprocessing = True
 
 # Hyperparameters
-epochs = 20
+epochs = 40
 batch_size = 128
 learning_rate = 0.0002
-params = 800
+params = 1200
 activation = tf.nn.relu
 
 # At which sample starts the prediction for the test data?
