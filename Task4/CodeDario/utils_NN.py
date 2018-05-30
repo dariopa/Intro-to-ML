714cc7eb677d0c0a165c749dc944a10973366f3b
dariopa <dariopa@ethz.ch>
2018-05-30 14:20:16 +0200

    new network

--- Task4/CodeDario/utils_NN.py
+++ Task4/CodeDario/utils_NN.py
@@ -82,13 +82,13 @@ class NeuralNetworks():
         h2 = fc_layer(h1, name='fc_2',
                     n_output_units=params,
                     activation_fn=activation)
-        # 3nd layer: FulCon_3
-        h3 = fc_layer(h2, name='fc_3',
-                    n_output_units=params,
-                    activation_fn=activation)
+        # # 3nd layer: FulCon_3
+        # h3 = fc_layer(h2, name='fc_3',
+        #             n_output_units=params,
+        #             activation_fn=activation)
         # Dropout
         keep_prob = tf.placeholder(tf.float32, name='fc_keep_prob')
-        h_drop = tf.nn.dropout(h3, keep_prob=keep_prob, name='dropout_layer')
+        h_drop = tf.nn.dropout(h2, keep_prob=keep_prob, name='dropout_layer')
 
         # logits layer: FulCon_3 (linear activation)
         logits = fc_layer(h_drop, name='fc_logits',
